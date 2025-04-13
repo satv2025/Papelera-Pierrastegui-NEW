e0eed81d39ec5da6a0aead49179562136dd1589d
SolArgentinoTV2025 <traplegionrecords2020@gmail.com>
2025-04-13 00:47:01 -0300

    act

--- assets/js/preciosdrop-cartonc.js
+++ assets/js/preciosdrop-cartonc.js
@@ -1,40 +1,34 @@
 document.addEventListener("DOMContentLoaded", function () {
     const precios = {
-        "100x20": { unidad: '10.000' },
-        "120x20": { unidad: '12.000' }
+        "100x20": '10.000',
+        "120x20": '12.000'
     };
-    
+
     const dropdownItems = document.querySelectorAll(".dropdown-menu li");
     const btn = document.querySelector(".dropdown-btn");
 
     const precioBox = document.getElementById("uni");
     const sinStock = document.getElementById("sin-stock");
 
-    const precio = document.getElementById("precio-50");
-    const precioMoneda = document.getElementById("precio-50-moneda");
+    const precioUnidad = document.getElementById("precio-unidad");
+    const precioUnidadMoneda = document.getElementById("precio-unidad-moneda");
 
     dropdownItems.forEach(item => {
         item.addEventListener("click", () => {
             const size = item.getAttribute("data-size");
             btn.textContent = item.textContent;
 
-            const data = precios[size];
+            const precio = precios[size];
 
-            if (data) {
-                const { u1 } = data;
+            if (precio && precio !== "none") {
+                precioUnidad.textContent = `$${precio}`;
+                precioUnidadMoneda.textContent = "ARS";
 
-                const isStock = !(u1.toLowerCase().includes("no está"));
-
-                if (isStock) {
-                    precio.textContent = `$${u1}`;
-                    precioMoneda.textContent = "ARS";
-
-                    sinStock.style.display = "none";
-                    precioBox.style.display = "block";
-                } else {
-                    precioBox.style.display = "none";
-                    sinStock.style.display = "block";
-                }
+                sinStock.style.display = "none";
+                precioBox.style.display = "block";
+            } else {
+                precioBox.style.display = "none";
+                sinStock.style.display = "block";
             }
         });
     });
