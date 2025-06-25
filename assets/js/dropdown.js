1e1c38ce7de33f593d0763c442bcff09f7a9e175
satv2025 <traplegionrecords2020@gmail.com>
2025-06-25 03:17:06 -0300

    Update dropdown.js

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -1,48 +1,42 @@
 document.addEventListener("DOMContentLoaded", function () {
+    // Código dropdown
     const dropdownBtn = document.querySelector(".dropdown-btn");
     const dropdownMenu = document.querySelector(".dropdown-menu");
     const sizeOptions = document.querySelectorAll(".dropdown-menu li");
     const uniSpan = document.getElementById('uni');
 
-    // Abrir/cerrar dropdown al hacer click en el botón
     dropdownBtn.addEventListener("click", function () {
         dropdownMenu.classList.toggle("show");
     });
 
-    // Acción al elegir una opción del dropdown
     sizeOptions.forEach(option => {
         option.addEventListener("click", function () {
             dropdownBtn.textContent = `Tamaño: ${this.dataset.size}`;
             dropdownMenu.classList.remove("show");
-            uniSpan.style.display = 'inline'; // Mostrar precios
+            uniSpan.style.display = 'inline';
         });
     });
 
-    // Cerrar dropdown si se hace clic fuera
     document.addEventListener("click", function (event) {
         if (!dropdownBtn.contains(event.target) && !dropdownMenu.contains(event.target)) {
             dropdownMenu.classList.remove("show");
         }
     });
-});
 
-   // Detectar "<a href>" y redirijir
+    // Código para cambiar href en footer
+    const urlMap = {
+      "https://www.facebook.com": "https://nueva-url-para-facebook.com",
+      "https://instagram.com": "https://nueva-url-para-instagram.com",
+      "https://twitter.com": "https://nueva-url-para-twitter.com",
+      "https://wa.me": "https://nueva-url-para-whatsapp.com"
+    };
 
-   // Mapa de URLs originales a nuevas URLs destino
-const urlMap = {
-  "https://www.facebook.com": "https://nueva-url-para-facebook.com",
-  "https://instagram.com": "https://nueva-url-para-instagram.com",
-  "https://twitter.com": "https://nueva-url-para-twitter.com",
-  "https://wa.me": "https://nueva-url-para-whatsapp.com"
-};
-
-document.querySelectorAll("footer a").forEach(link => {
-  for (const originalUrl in urlMap) {
-    // Si el href del link empieza con alguna de las URLs originales
-    if (link.href.startsWith(originalUrl)) {
-      // Cambia el href a la nueva URL correspondiente
-      link.href = urlMap[originalUrl];
-      break; // Salimos del bucle al encontrar coincidencia
-    }
-  }
+    document.querySelectorAll("footer a").forEach(link => {
+      for (const originalUrl in urlMap) {
+        if (link.href.startsWith(originalUrl)) {
+          link.href = urlMap[originalUrl];
+          break;
+        }
+      }
+    });
 });
