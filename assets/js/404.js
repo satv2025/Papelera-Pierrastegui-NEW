97c39488d004847206a3252adcbf9c6bd9728e5e
satv2025 <traplegionrecords2020@gmail.com>
2025-07-28 16:53:22 -0300

    act

---
+++ assets/js/404.js
@@ -0,0 +1,18 @@
+window.addEventListener("DOMContentLoaded", () => {
+    const path = window.location.pathname;
+    const title = document.getElementById("error-title");
+    const description = document.getElementById("error-description");
+    const reasons = document.getElementById("motivos");
+
+    if (path.startsWith("/producto")) {
+        title.textContent = "Este producto no existe o está siendo agregado";
+        description.textContent =
+            "El producto o subcategoría que estás buscando no se encuentra disponible.";
+        reasons.innerHTML = `
+      <li>El producto fue eliminado.</li>
+      <li>La URL es incorrecta.</li>
+      <li>Estamos en proceso de cargar este producto.</li>
+      <li>La subcategoría puede haber sido modificada o borrada.</li>
+    `;
+    }
+});
