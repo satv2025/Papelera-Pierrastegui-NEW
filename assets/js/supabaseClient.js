fea0b421ffd2c98d516be3b410ab223a0c038c34
satv2025 <traplegionrecords2020@gmail.com>
2026-01-28 00:29:06 -0300

    act

--- assets/js/supabaseClient.js
+++ assets/js/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
 export const supabase = createClient(
-    'https://pkptcnxgetrvmblphucg.supabase.co',
+    'https://login.papelerapierrastegui.com.ar',
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBrcHRjbnhnZXRydm1ibHBodWNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA3MzQ2ODMsImV4cCI6MjA3NjMxMDY4M30.IIJjxPA6vzZDaxwYnRhgERdCi-G-CG0-3y1EIlYL7v4'
 );
 
